c82d0e1633dfac9a8638c13d66ed4e1b0eae3762
Abdul Majid <abdulmajidcse@gmail.com>
2024-01-29 19:24:56 +0600

    Inheritance practise.

--- inheritance.py
+++ inheritance.py
@@ -6,11 +6,32 @@ class Animal:
     def eat(self):
         return 'I\'m eating...'
 
-class Dog(Animal):
+
+class Animal_Food:
+    def __init__(self, food_name):
+        self.food_name = food_name
+    
+    def get_food(self):
+        return self.food_name
+
+
+class Cat(Animal, Animal_Food):
+    def __init__(self, name, color, food_name):
+        Animal.__init__(self, name, color)
+        Animal_Food.__init__(self, food_name)
+
     def about(self):
         return '{0} has four legs and it\'s {1} color.'.format(self.name, self.color)
 
-dog = Dog('Dog', 'white')
 
-print(dog.about())
-print('The dog object is a instance of Animal class or not:', isinstance(dog, Animal))
+cat = Cat('Mina', 'white', 'Milk')
+
+print(cat.name, 'is eating ', cat.get_food())
+
+print('--------------------------------------------')
+
+print(cat.about())
+
+print('---------------------------------------')
+
+print('The', cat.name, 'object is a instance of Animal class or not:', isinstance(cat, Animal))
