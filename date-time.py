8d40115d68c4da7749ef86228dbd2f72a385d29e
Abdul Majid <abdulmajidcse@gmail.com>
2024-11-02 10:35:59 +0600

    datetime

---
+++ date-time.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+today = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+
+print(today)
+
+date1 = datetime(2024, 11, 2)
+date2 = datetime(2020, 10, 15)
+
+diff = date1 - date2
+
+print(diff.days)
